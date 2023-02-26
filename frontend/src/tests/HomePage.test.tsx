e7196ba7b74dda8ed529c91dea755f5c91ea2475
Shea Meyers <shea.meyers@gmail.com>
2023-02-26 13:27:12 +0100

    Update unit test

--- frontend/src/tests/HomePage.test.tsx
+++ frontend/src/tests/HomePage.test.tsx
@@ -3,6 +3,10 @@ import HomePage from '../components/HomePage';
 
 test('renders homepage', () => {
   render(<HomePage />);
-  const linkElement = screen.getByText(/worker coordinator/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/worker coordinator/i);
+  const signUpButton = screen.getByRole('button', { name: 'Sign Up' });
+  const signInButton = screen.getByRole('button', { name: 'Sign In' });
+  expect(titleElement).toBeInTheDocument();
+  expect(signUpButton).toBeInTheDocument();
+  expect(signInButton).toBeInTheDocument();
 });
