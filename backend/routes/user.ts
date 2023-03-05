b85fec14f6731c296afbbb2958c1005b91d19ef9
Shea Meyers <shea.meyers@gmail.com>
2023-03-05 15:26:58 +0100

    Check if username or company are already taken when signing up

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -9,6 +9,14 @@ userRouter.post('/sign-up', async (req: Request, res: Response) => {
 
     const { username, password, companyName } = req.body
 
+    if (await prisma.user.findUnique({ where: { name: username } })) {
+        return res.status(400).send(`User already exists with username ${username}`)
+    }
+
+    if (await prisma.company.findUnique({ where: { name: companyName } })) {
+        return res.status(400).send(`Company already exists with username ${companyName}`)
+    }
+
     const encryptedPassword = await bcrypt.hash(password, 10)
 
     const company: Company = await prisma.company.create({
