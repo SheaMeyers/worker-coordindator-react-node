a4c5408361273e752f5a1e7add8c91d027994534
Shea Meyers <shea.meyers@gmail.com>
2023-04-30 15:19:05 +0200

    Improve styling of messages

--- frontend/src/components/base/AdminHome.tsx
+++ frontend/src/components/base/AdminHome.tsx
@@ -1,43 +1,56 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import AddWorkerModal from "../AddWorkerModal";
 import Button from "@mui/material/Button";
 import LogoutButton from "./LogoutButton";
 import "../../styles/HomePage.css";
 import { UsersWithMessages } from "../../interfaces";
 import { getUsersWithMessages } from "../../api";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Card,
+  CardContent,
+  Typography,
+} from "@mui/material";
 
 const AdminHome = () => {
-  const [usersWithMessages, setUsersWithMessages] = useState<UsersWithMessages>([])
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-  const handleClose = () => setIsOpen(false)
+  const [usersWithMessages, setUsersWithMessages] = useState<UsersWithMessages>(
+    []
+  );
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const handleClose = () => setIsOpen(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await getUsersWithMessages()
-      setUsersWithMessages(response)
-    }
-    fetchData()
-  }, [])
+      const response = await getUsersWithMessages();
+      setUsersWithMessages(response);
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
       <LogoutButton />
       <Button onClick={(_) => setIsOpen(true)}>Add Worker</Button>
       <h1>Admin Homepage</h1>
-      {usersWithMessages.map(userWithMessages => (
-        <div style={{ backgroundColor: 'lightgray'}}>
-        <h2>{userWithMessages.username}</h2>
-        {userWithMessages.Message.map(message => (
-          <h3>{message.content}</h3>
-        ))}
-        </div>
+      {usersWithMessages.map((userWithMessages) => (
+        <Accordion>
+          <AccordionSummary>
+            <Typography>{userWithMessages.username}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {userWithMessages.Message.map((message) => (
+              <Card>
+                <CardContent>{message.content}</CardContent>
+              </Card>
+            ))}
+          </AccordionDetails>
+        </Accordion>
       ))}
-      <AddWorkerModal
-        isOpen={isOpen}
-        handleClose={handleClose}
-      /> 
+      <AddWorkerModal isOpen={isOpen} handleClose={handleClose} />
     </>
-  )
-}
+  );
+};
 
-export default AdminHome
+export default AdminHome;
